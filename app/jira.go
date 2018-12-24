5bfa45a06360e67e0c990be7c41f231e4ada8896
Ivan Volkov <b0s.at.works@gmail.com>
2018-12-24 11:29:32 +0300

    BOA-27 Log map marshaling errors; Log asserts error; Make conditionally reversed if block and move inner code out

--- app/jira.go
+++ app/jira.go
@@ -2,8 +2,8 @@ package app
 
 import (
 	"fmt"
-
 	"github.com/andygrunwald/go-jira"
+	"github.com/sirupsen/logrus"
 )
 
 // IssuesSearch searches Issues in all sprints which opened now and returning list with issues in this sprints list
@@ -35,39 +35,51 @@ func (a *App) IssuesSearch() ([]jira.Issue, *jira.Response, error) {
 
 func (a *App) IssueTimeExcisionWWithTimeCompare(issue jira.Issue, rowIndex int) (string, error) {
 	var listRow string
-	if issue.Fields.TimeSpent > issue.Fields.TimeOriginalEstimate {
+	if issue.Fields.TimeSpent < issue.Fields.TimeOriginalEstimate {
+		return listRow, nil
+	}
 
-		ts, err := a.SecondsToClockTime(issue.Fields.TimeSpent)
-		te, err := a.SecondsToClockTime(issue.Fields.TimeOriginalEstimate)
-		if err != nil {
-			return listRow, fmt.Errorf("time conversion: regexp error: %v", err)
-
-		}
-
-		listRow = fmt.Sprintf("%[1]d. <https://theflow.atlassian.net/browse/%[2]s|%[2]s - %[3]s>: %[4]v из %[5]v\n",
-			rowIndex, issue.Key, issue.Fields.Summary, ts, te,
-		)
+	ts, err := a.SecondsToClockTime(issue.Fields.TimeSpent)
+	te, err := a.SecondsToClockTime(issue.Fields.TimeOriginalEstimate)
+	if err != nil {
+		return listRow, fmt.Errorf("time conversion: regexp error: %v", err)
 
 	}
+
+	listRow = fmt.Sprintf("%[1]d. <https://theflow.atlassian.net/browse/%[2]s|%[2]s - %[3]s>: %[4]v из %[5]v\n",
+		rowIndex, issue.Key, issue.Fields.Summary, ts, te,
+	)
 
 	return listRow, nil
 }
 
 func (a *App) IssueTimeExceededNoTimeRange(issue jira.Issue, rowIndex int) string {
 	var listRow string
-	if issue.Fields.TimeTracking.RemainingEstimateSeconds == 0 {
-
-		var developer string
-		if developerMap, err := issue.Fields.Unknowns.MarshalMap("customfield_10026"); err == nil && developerMap != nil {
-			developer = developerMap["displayName"].(string)
-		} else {
-			developer = "No developer"
-		}
-
-		listRow = fmt.Sprintf("%[1]d. %[2]s - <https://theflow.atlassian.net/browse/%[3]s|%[3]s - %[4]s>: _%[5]s_\n",
-			rowIndex, developer, issue.Key, issue.Fields.Summary, issue.Fields.Status.Name,
-		)
+	if issue.Fields.TimeTracking.RemainingEstimateSeconds != 0 {
+		return listRow
 	}
+
+	var developer = "No developer"
+	developerMap, err := issue.Fields.Unknowns.MarshalMap("customfield_10026")
+	if err != nil {
+		logrus.
+			WithError(err).
+			WithField("developerMap", developerMap).
+			Error("can't make customfield_10026 map marshaling")
+	} else if developerMap != nil {
+		displayName, ok := developerMap["displayName"].(string)
+		if !ok {
+			logrus.
+				WithField("displayName", developerMap["displayName"]).
+				Error("can't assert to string map displayName field")
+		} else {
+			developer = displayName
+		}
+	}
+
+	listRow = fmt.Sprintf("%[1]d. %[2]s - <https://theflow.atlassian.net/browse/%[3]s|%[3]s - %[4]s>: _%[5]s_\n",
+		rowIndex, developer, issue.Key, issue.Fields.Summary, issue.Fields.Status.Name,
+	)
 
 	return listRow
 }
