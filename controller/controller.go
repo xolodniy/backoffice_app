7fb04d2e9bec5fde80e3e29fd94a6ea5644c4edd
Roman Vlasenko <roman.vlasenko@theflow.global>
2019-02-18 17:29:06 +0700

    BOA-41 added new endpoint for sprint report

--- controller/controller.go
+++ controller/controller.go
@@ -1,6 +1,9 @@
 package controller
 
 import (
+	"bytes"
+	"github.com/sirupsen/logrus"
+	"io/ioutil"
 	"net/http"
 
 	"backoffice_app/app"
@@ -47,7 +50,31 @@ func (c *Controller) initRoutes() {
 	c.Gin.GET("/healthcheck", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{"result": "ok"})
 	})
+	c.Gin.POST("/sprintreport", c.sprintReport)
 
 	c.Gin.POST("/api/v1/git/onevent/push", c.gitHandlerOnEventPush)
 
+}
+
+func (c *Controller) sprintReport(ctx *gin.Context) {
+	body, err := ioutil.ReadAll(ctx.Request.Body)
+	if err != nil {
+		logrus.Debug(err)
+	}
+	ctx.Request.Body = ioutil.NopCloser(bytes.NewBuffer([]byte(body))) //return response body back
+	timestamp := ctx.GetHeader("X-Slack-Request-Timestamp")
+	signature := ctx.GetHeader("X-Slack-Signature")
+	userId := ctx.PostForm("user_id")
+	text := ctx.PostForm("text")
+	if c.App.CheckSignature(signature, []byte("v0:"+timestamp+":"+string(body))) {
+		go func() {
+			err := c.App.ReportSprintsIsuues(text, userId)
+			if err != nil {
+				c.App.Slack.SendMessage(err.Error(), userId, true)
+			}
+		}()
+		ctx.JSON(http.StatusOK, "ok, wait for report")
+		return
+	}
+	ctx.JSON(http.StatusOK, "Fail to authorize")
 }
