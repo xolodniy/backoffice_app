317ededab931ea3fdf629eafee8c82d26ce622fc
Roman Vlasenko <roman.vlasenko@theflow.global>
2019-03-11 19:37:07 +0700

    BOA-10 add users response struct and UserEmailByName to fine email by user name

--- services/slack/slack.go
+++ services/slack/slack.go
@@ -41,10 +41,32 @@ type FilesResponse struct {
 	} `json:"paging"`
 }
 
+// UsersResponse is struct of users.list answer (https://api.slack.com/methods/users.list)
+type UsersResponse struct {
+	Ok      bool     `json:"ok"`
+	Error   string   `json:"error"`
+	Warning string   `json:"warning"`
+	Members []Member `json:"members"`
+	Paging  struct {
+		Count int `json:"count"`
+		Total int `json:"total"`
+		Page  int `json:"page"`
+		Pages int `json:"pages"`
+	} `json:"paging"`
+}
+
 // Files piece of FilesResponse struct for files api answer
 type Files struct {
 	ID   string  `json:"id"`
 	Size float64 `json:"size"`
+}
+
+// Member is user object contains information about a member https://api.slack.com/types/user
+type Member struct {
+	Name    string `json:"name"`
+	Profile struct {
+		Email string `json:"email"`
+	} `json:"profile"`
 }
 
 // New creates new slack
@@ -233,4 +255,41 @@ func (s *Slack) UploadFile(channel, contentType string, file *bytes.Buffer) erro
 	}
 
 	return nil
+}
+
+func (s *Slack) UserEmailByName(username string) (string, error) {
+	for i := 0; ; i++ {
+		urlStr := fmt.Sprintf("%s/users.list?token=%s&page=%v", s.APIURL, s.InToken, i)
+
+		req, err := http.NewRequest("GET", urlStr, nil)
+		if err != nil {
+			return "", err
+		}
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		usersResp := UsersResponse{}
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return "", err
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return "", err
+		}
+		if err := json.Unmarshal(body, &usersResp); err != nil {
+			return "", err
+		}
+		if !usersResp.Ok {
+			return "", fmt.Errorf(usersResp.Error)
+		}
+		for _, member := range usersResp.Members {
+			if member.Name == username {
+				return member.Profile.Email, nil
+			}
+		}
+		if usersResp.Paging.Pages == i {
+			break
+		}
+	}
+	return "", nil
 }
