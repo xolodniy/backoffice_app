2295cb8eb1a5d1f40e65a9474ec9e9f47282d550
Roman Vlasenko <roman.vlasenko@theflow.global>
2019-03-11 16:16:11 +0700

    BOA-77 add ArtDirector variable to config, slack for mention in message designer about tasks with closed subtasks and in design review

--- config/config.go
+++ config/config.go
@@ -55,6 +55,7 @@ type Slack struct {
 	OutToken       string
 	BotName        string
 	ProjectManager string
+	ArtDirector    string
 	APIURL         string
 	TotalVolume    float64
 	RestVolume     float64
