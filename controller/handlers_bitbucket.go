00bd177841b576c4a121b26e83edc8d912318cf0
Roman Vlasenko <roman.vlasenko@theflow.global>
2019-06-14 08:16:34 +0300

    BOA-140 add handler for pull request merged webhook

--- controller/handlers_bitbucket.go
+++ controller/handlers_bitbucket.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"bytes"
+	"io/ioutil"
 	"net/http"
 
 	"backoffice_app/services/bitbucket"
@@ -18,5 +20,28 @@ func (c *Controller) commitPushed(ctx *gin.Context) {
 		return
 	}
 	go c.App.CreateBranchPullRequest(repoPushPayload)
+	ctx.JSON(http.StatusOK, gin.H{"result": "ok"})
+}
+
+func (c *Controller) pullRequestMerged(ctx *gin.Context) {
+	body, err := ioutil.ReadAll(ctx.Request.Body)
+	if err != nil {
+		ctx.JSON(http.StatusForbidden, gin.H{
+			"error": "Fail to authorize",
+		})
+		ctx.Abort()
+		return
+	}
+	ctx.Request.Body = ioutil.NopCloser(bytes.NewBuffer([]byte(body))) //return response body back
+	logrus.Debug(string(body))
+	pullRequestMergedPayload := bitbucket.PullRequestMergedPayload{}
+	err = ctx.ShouldBindJSON(&pullRequestMergedPayload)
+	if err != nil {
+		ctx.String(http.StatusBadRequest, "error")
+		logrus.WithError(err).Error("can't bind json from bitnucket webhook")
+		return
+	}
+	logrus.Debug(pullRequestMergedPayload)
+	go c.App.CheckPullRequestsConflicts(pullRequestMergedPayload)
 	ctx.JSON(http.StatusOK, gin.H{"result": "ok"})
 }
