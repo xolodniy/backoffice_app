7a4a926dff1852c6cdfbd35d561559bfd05669ed
Roman Vlasenko <roman.vlasenko@atnr.pro>
2019-12-25 08:12:38 +0300

    BOA-217 refactor migration

--- etc/migrations/0003.sql
+++ etc/migrations/0003.sql
@@ -1,22 +1,15 @@
 CREATE TABLE forgotten_pull_requests
 (
-    id              SERIAL                      NOT NULL PRIMARY KEY,
-    pull_request_id SERIAL                      NOT NULL,
-    title           VARCHAR                     NOT NULL,
-    author          VARCHAR                     NOT NULL,
+    pull_request_id INT                         NOT NULL,
     repo_slug       VARCHAR                     NOT NULL,
-    href            VARCHAR                     NOT NULL,
-    last_activity   TIMESTAMP WITHOUT TIME ZONE,
     created_at      TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
-    updated_at      TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
+    PRIMARY KEY (pull_request_id, repo_slug)
 );
 
 CREATE TABLE forgotten_branches
 (
-    id         SERIAL                      NOT NULL PRIMARY KEY,
-    name       VARCHAR                     NOT NULL,
-    author     VARCHAR                     NOT NULL,
-    repo_slug  VARCHAR                     NOT NULL,
-    href       VARCHAR                     NOT NULL,
-    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
+    name          VARCHAR                     NOT NULL,
+    repo_slug     VARCHAR                     NOT NULL,
+    created_at    TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (name, repo_slug)
 );
