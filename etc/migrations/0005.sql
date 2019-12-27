1f3be0b82f2b2c6e921122e54aff095fbc8aad86
Roman Vlasenko <roman.vlasenko@atnr.pro>
2019-12-27 07:39:47 +0300

    BOA-217 rename migration

---
+++ etc/migrations/0005.sql
@@ -0,0 +1,15 @@
+CREATE TABLE forgotten_pull_requests
+(
+    pull_request_id INT                         NOT NULL,
+    repo_slug       VARCHAR                     NOT NULL,
+    created_at      TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (pull_request_id, repo_slug)
+);
+
+CREATE TABLE forgotten_branches
+(
+    name       VARCHAR                     NOT NULL,
+    repo_slug  VARCHAR                     NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (name, repo_slug)
+);
